6df15743ff962e41d9ac9b6b32e8c35ee04dd690
marco <sciaini.marco@gmail.com>
2018-07-04 14:38:58 +0200

    asdasd

--- figures.R
+++ figures.R
@@ -2,3 +2,5 @@
 util_facetplot(list("a) Low Autocorrelation"    = landscape_low_autocorrelation,
                     "b) Medium Autocorrelation" = landscape_medium_autocorrelation,
                     "c) High Autocorrelation"   = landscape_high_autocorrelation))
+
+dfsdf

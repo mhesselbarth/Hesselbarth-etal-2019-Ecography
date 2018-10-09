1ff2cee9a6459f064bfad9ff23732328b2c129e0
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-10-09 16:59:53 +0200

    Results

--- 2_Experiment/4_compare_values.R
+++ 2_Experiment/4_compare_values.R
@@ -3,7 +3,11 @@
 purrr::walk(list.files(path = "1_Setup_Functions", pattern = ".R", full.names = TRUE), 
             function(x) source(x))
 
-#### 2. Import results #### 
+absolute_metrics <- c("ca", "ndca", "np", "pafrac", "pr", "ta", "tca", "te")
+
+simulation_design$id <- rep(1:(nrow(simulation_design)/10), times = 10)
+
+#### 1. Low AC ####
 
 # Low AC
 sampling_low_ac <- readr::read_rds(path = paste0(getwd(), 
@@ -12,137 +16,184 @@ sampling_low_ac <- readr::read_rds(path = paste0(getwd(),
 true_value_low_ac <- readr::read_rds(path = paste0(getwd(), 
                                                    "/3_Output/true_value_low_ac.rds"))
 
-# Medium AC
-sampling_medium_ac <- readr::read_rds(path = paste0(getwd(),
-                                                   "/3_Output/sampling_medium_ac.rds"))
+for(i in 1:length(true_value_low_ac)) {
+  true_value_low_ac[[i]] <- dplyr::filter(true_value_low_ac[[i]], 
+                level != "patch" & !(metric %in% absolute_metrics))
+  
+  true_value_low_ac[[i]] <- dplyr::mutate(true_value_low_ac[[i]], layer = i)
+}
+
+true_value_low_ac <- dplyr::bind_rows(true_value_low_ac)
+
+for(i in 1:length(sampling_low_ac)) {
+  for(j in 1:length(sampling_low_ac[[i]]$metrics)) {
+    sampling_low_ac[[i]]$metrics[[j]] <-  dplyr::filter(sampling_low_ac[[i]]$metrics[[j]],
+                                                        level != "patch" & !(metric %in% absolute_metrics))
+
+    sampling_low_ac[[i]]$metrics[[j]] <- dplyr::mutate(sampling_low_ac[[i]]$metrics[[j]], 
+                                                       landscape_id = simulation_design$i[i],
+                                                       simulation_id = simulation_design$id[i])
+  }
+}
+
+deviation_low_ac <- bind_rows(sampling_low_ac) %>% 
+  unnest(metrics) %>%
+  dplyr::group_by(simulation_id, landscape_id, level, class, metric) %>%
+  dplyr::summarise(n = n(),
+                   mean = mean(value, na.rm = TRUE),
+                   sd = sd(value, na.rm = TRUE),
+                   ci = 1.96 *  (sd / sqrt(n)),
+                   lo = mean - ci,
+                   hi = mean + ci) %>%
+  dplyr::left_join(true_value_low_ac, 
+                   by = c("landscape_id" = "layer", 
+                          "level" = "level",
+                          "class" = "class",
+                          "metric" = "metric")) %>%
+  dplyr::mutate(inside_ci = dplyr::case_when(lo < value & value < hi ~ 1, 
+                                             lo > value | hi < value ~ 0), 
+                inside_ci = dplyr::case_when(is.na(inside_ci) ~ 0, 
+                                             !is.na(inside_ci) ~ inside_ci)) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                   by = "metric") %>% 
+  dplyr::group_by(simulation_id, level, class, type) %>% 
+  dplyr::summarise(n = n(), 
+                   correct = mean(inside_ci, na.rm = TRUE), 
+                   false = 1 - correct)
+
+rm(sampling_low_ac, true_value_low_ac)
+
+#### 2. Medium AC ####
+
+# medium AC
+sampling_medium_ac <- readr::read_rds(path = paste0(getwd(), 
+                                                 "/3_Output/sampling_medium_ac.rds"))
 
 true_value_medium_ac <- readr::read_rds(path = paste0(getwd(), 
                                                    "/3_Output/true_value_medium_ac.rds"))
 
-# High AC
-sampling_high_ac <- readr::read_rds(path = paste0(getwd(),
+for(i in 1:length(true_value_medium_ac)) {
+  true_value_medium_ac[[i]] <- dplyr::filter(true_value_medium_ac[[i]], 
+                                          level != "patch" & !(metric %in% absolute_metrics))
+  
+  true_value_medium_ac[[i]] <- dplyr::mutate(true_value_medium_ac[[i]], layer = i)
+}
+
+true_value_medium_ac <- dplyr::bind_rows(true_value_medium_ac)
+
+for(i in 1:length(sampling_medium_ac)) {
+  for(j in 1:length(sampling_medium_ac[[i]]$metrics)) {
+    sampling_medium_ac[[i]]$metrics[[j]] <-  dplyr::filter(sampling_medium_ac[[i]]$metrics[[j]],
+                                                        level != "patch" & !(metric %in% absolute_metrics))
+    
+    sampling_medium_ac[[i]]$metrics[[j]] <- dplyr::mutate(sampling_medium_ac[[i]]$metrics[[j]], 
+                                                       landscape_id = simulation_design$i[i],
+                                                       simulation_id = simulation_design$id[i])
+  }
+}
+
+deviation_medium_ac <- bind_rows(sampling_medium_ac) %>% 
+  unnest(metrics) %>%
+  dplyr::group_by(simulation_id, landscape_id, level, class, metric) %>%
+  dplyr::summarise(n = n(),
+                   mean = mean(value, na.rm = TRUE),
+                   sd = sd(value, na.rm = TRUE),
+                   ci = 1.96 *  (sd / sqrt(n)),
+                   lo = mean - ci,
+                   hi = mean + ci) %>%
+  dplyr::left_join(true_value_medium_ac, 
+                   by = c("landscape_id" = "layer", 
+                          "level" = "level",
+                          "class" = "class",
+                          "metric" = "metric")) %>%
+  dplyr::mutate(inside_ci = dplyr::case_when(lo < value & value < hi ~ 1, 
+                                             lo > value | hi < value ~ 0), 
+                inside_ci = dplyr::case_when(is.na(inside_ci) ~ 0, 
+                                             !is.na(inside_ci) ~ inside_ci)) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                   by = "metric") %>% 
+  dplyr::group_by(simulation_id, level, class, type) %>% 
+  dplyr::summarise(n = n(), 
+                   correct = mean(inside_ci, na.rm = TRUE), 
+                   false = 1 - correct)
+
+rm(sampling_medium_ac, true_value_medium_ac)
+
+#### 3. High AC ####
+
+# high AC
+sampling_high_ac <- readr::read_rds(path = paste0(getwd(), 
                                                  "/3_Output/sampling_high_ac.rds"))
 
 true_value_high_ac <- readr::read_rds(path = paste0(getwd(), 
                                                    "/3_Output/true_value_high_ac.rds"))
 
-
-
-
-#### 3. Select only comparable metrics
-
-absolute_metrics <- c("ca", "ndca", "np", "pafrac", "pr", "ta", "tca", "te")
-
-# Low AC
-filtered_true_value_low_ac <- purrr::map(seq_along(true_value_low_ac), function(current_landscape) { 
+for(i in 1:length(true_value_high_ac)) {
+  true_value_high_ac[[i]] <- dplyr::filter(true_value_high_ac[[i]], 
+                                          level != "patch" & !(metric %in% absolute_metrics))
   
-  df_filtered <- dplyr::filter(true_value_low_ac[[current_landscape]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = current_landscape)
-})
+  true_value_high_ac[[i]] <- dplyr::mutate(true_value_high_ac[[i]], layer = i)
+}
 
-filtered_sampling_low_ac <- purrr::map(seq_along(sampling_low_ac), function(current_sampling) { 
-  
-  df_filtered <- dplyr::filter(sampling_low_ac[[current_sampling]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = simulation_design$i[current_sampling])
-})
+true_value_high_ac <- dplyr::bind_rows(true_value_high_ac)
 
+for(i in 1:length(sampling_high_ac)) {
+  for(j in 1:length(sampling_high_ac[[i]]$metrics)) {
+    sampling_high_ac[[i]]$metrics[[j]] <-  dplyr::filter(sampling_high_ac[[i]]$metrics[[j]],
+                                                        level != "patch" & !(metric %in% absolute_metrics))
+    
+    sampling_high_ac[[i]]$metrics[[j]] <- dplyr::mutate(sampling_high_ac[[i]]$metrics[[j]], 
+                                                       landscape_id = simulation_design$i[i],
+                                                       simulation_id = simulation_design$id[i])
+  }
+}
 
-# Medium AC
-filtered_true_value_medium_ac <- purrr::map(seq_along(true_value_medium_ac), function(current_landscape) { 
-  
-  df_filtered <- dplyr::filter(true_value_medium_ac[[current_landscape]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = current_landscape)
-})
+deviation_high_ac <- bind_rows(sampling_high_ac) %>% 
+  unnest(metrics) %>%
+  dplyr::group_by(simulation_id, landscape_id, level, class, metric) %>%
+  dplyr::summarise(n = n(),
+                   mean = mean(value, na.rm = TRUE),
+                   sd = sd(value, na.rm = TRUE),
+                   ci = 1.96 *  (sd / sqrt(n)),
+                   lo = mean - ci,
+                   hi = mean + ci) %>%
+  dplyr::left_join(true_value_high_ac, 
+                   by = c("landscape_id" = "layer", 
+                          "level" = "level",
+                          "class" = "class",
+                          "metric" = "metric")) %>%
+  dplyr::mutate(inside_ci = dplyr::case_when(lo < value & value < hi ~ 1, 
+                                             lo > value | hi < value ~ 0), 
+                inside_ci = dplyr::case_when(is.na(inside_ci) ~ 0, 
+                                             !is.na(inside_ci) ~ inside_ci)) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                   by = "metric") %>% 
+  dplyr::group_by(simulation_id, level, class, type) %>% 
+  dplyr::summarise(n = n(), 
+                   correct = mean(inside_ci, na.rm = TRUE), 
+                   false = 1 - correct)
 
-filtered_sampling_medium_ac <- purrr::map(seq_along(sampling_medium_ac), function(current_sampling) { 
-  
-  df_filtered <- dplyr::filter(sampling_medium_ac[[current_sampling]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = simulation_design$i[current_sampling])
-})
-
-
-# High AC
-filtered_true_value_high_ac <- purrr::map(seq_along(true_value_high_ac), function(current_landscape) { 
-  
-  df_filtered <- dplyr::filter(true_value_high_ac[[current_landscape]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = current_landscape)
-})
-
-filtered_sampling_high_ac <- purrr::map(seq_along(sampling_high_ac), function(current_sampling) { 
-  
-  df_filtered <- dplyr::filter(sampling_high_ac[[current_sampling]], 
-                               level != "patch" & !(metric %in% absolute_metrics))
-  
-  dplyr::mutate(df_filtered, i = simulation_design$i[current_sampling])
-})
-
-#### 4. Calculate sample estimation #### 
-
-purrr::map(filtered_sampling_high_ac, function(x) { 
-  estimate_landscape_value(x)
-})
-
-
-# Low AC
-percentage_low_ac <- sampling_low_ac %>%
-  estimate_landscape_value() %>%
-  dplyr::left_join(true_value_low_ac, 
-                   by = c("simulation_run" = "layer", "metric")) %>% 
-  dplyr::mutate(within_ci = dplyr::case_when(value > lo & value < hi ~ 1, 
-                                             value < lo ~ 0, 
-                                             value > hi ~ 0)) %>%
-  dplyr::group_by(simulation_design) %>%
-  dplyr::summarise(n = (sum(within_ci) / n()) * 100)
-
-# Medium AC
-percentage_medium_ac <- sampling_medium_ac %>%
-  estimate_landscape_value() %>%
-  dplyr::left_join(true_value_medium_ac,
-                   by = c("simulation_run" = "layer", "metric")) %>%
-  dplyr::mutate(within_ci = dplyr::case_when(value > lo & value < hi ~ 1,
-                                             value < lo ~ 0,
-                                             value > hi ~ 0)) %>%
-  dplyr::group_by(simulation_design) %>%
-  dplyr::summarise(n = (sum(within_ci) / n()) * 100)
-
-# High AC
-percentage_high_ac <- sampling_high_ac %>%
-  estimate_landscape_value() %>%
-  dplyr::left_join(true_value_high_ac,
-                   by = c("simulation_run" = "layer", "metric")) %>%
-  dplyr::mutate(within_ci = dplyr::case_when(value > lo & value < hi ~ 1,
-                                             value < lo ~ 0,
-                                             value > hi ~ 0)) %>%
-  dplyr::group_by(simulation_design) %>%
-  dplyr::summarise(n = (sum(within_ci) / n()) * 100)
+rm(sampling_high_ac, true_value_high_ac)
 
 #### 4. Save Results ####
 
+overwrite <- FALSE
+
 # Low AC
-UtilityFunctions::Save.Function.rds(object = percentage_low_ac, 
-                                    filename = "percentage_low_ac.rds", 
-                                    path = paste0(getwd(), "/4_Results"), 
-                                    overwrite = FALSE)
+UtilityFunctions::save_rds(object = deviation_low_ac, 
+                           filename = "deviation_low_ac.rds", 
+                           path = paste0(getwd(), "/3_Output"),
+                           overwrite = overwrite)
 
 # Medium AC
-UtilityFunctions::Save.Function.rds(object = sampling_medium_ac, 
-                                    filename = "percentage_medium_ac.rds", 
-                                    path = paste0(getwd(), "/4_Results"),
-                                    overwrite = FALSE)
+UtilityFunctions::save_rds(object = deviation_medium_ac, 
+                           filename = "deviation_medium_ac.rds", 
+                           path = paste0(getwd(), "/3_Output"),
+                           overwrite = overwrite)
 
 # High AC
-UtilityFunctions::Save.Function.rds(object = percentage_high_ac, 
-                                    filename = "percentage_high_ac.rds", 
-                                    path = paste0(getwd(), "/4_Results"),
-                                    overwrite = FALSE)
+UtilityFunctions::save_rds(object = deviation_high_ac, 
+                           filename = "deviation_high_ac.rds", 
+                           path = paste0(getwd(), "/3_Output"),
+                           overwrite = overwrite)
 

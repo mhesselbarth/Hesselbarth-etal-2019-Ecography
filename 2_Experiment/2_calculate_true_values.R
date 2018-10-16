aba93d315f8b685861f74bc54f935e2ab64a636e
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-10-16 08:13:15 +0200

    Slightly different ordering

--- 2_Experiment/2_calculate_true_values.R
+++ 2_Experiment/2_calculate_true_values.R
@@ -3,72 +3,65 @@
 purrr::walk(list.files(path = "1_Setup_Functions", pattern = ".R", full.names = TRUE), 
             function(x) source(x))
 
-#### 2. Import landscapes #### 
+overwrite <- FALSE
 
-# Low AC
+#### 2. Low AC ####
 landscapes_low_ac <- readr::read_rds(paste0(getwd(), 
-                                            "/3_Output/landscapes_low_ac.rds"))
+                                            "/3_Output/landscapes_low_ac_50.rds"))
 
-# Medium AC
-landscapes_medium_ac <- readr::read_rds(paste0(getwd(), 
-                                               "/3_Output/landscapes_medium_ac.rds"))
-
-# High AC
-landscapes_high_ac <- readr::read_rds(paste0(getwd(), 
-                                             "/3_Output/landscapes_high_ac.rds"))
-
-
-#### 3. Calculate "real" values ####
-
-# Low AC
 true_value_low_ac <- clustermq::Q(fun = calculate_lsm_helper,
                                   i = seq_along(landscapes_low_ac), 
                                   const = list(landscape = landscapes_low_ac,
-                                               what = "all"),
+                                               what =  c("class", "landscape")),
                                   n_jobs = length(landscapes_low_ac), 
                                   template = list(queue = "mpi", 
                                                   walltime = "06:00", 
                                                   processes = 1))
 
-# Medium AC
+UtilityFunctions::save_rds(object = true_value_low_ac,
+                           filename = "true_value_low_ac_50.rds", 
+                           path = paste0(getwd(), "/3_Output"), 
+                           overwrite = overwrite)
+
+# rm(landscapes_low_ac)
+
+#### 3. Medium AC ####
+landscapes_medium_ac <- readr::read_rds(paste0(getwd(), 
+                                               "/3_Output/landscapes_medium_ac_50.rds"))
+
 true_value_medium_ac <- clustermq::Q(fun = calculate_lsm_helper,
                                      i = seq_along(landscapes_medium_ac), 
                                      const = list(landscape = landscapes_medium_ac,
-                                                  what = "all"),
+                                                  what = c("class", "landscape")),
                                      n_jobs = length(landscapes_medium_ac), 
                                      template = list(queue = "mpi", 
                                                      walltime = "06:00", 
                                                      processes = 1))
 
-# High AC
+UtilityFunctions::save_rds(object = true_value_medium_ac,
+                           filename = "true_value_medium_ac_50.rds", 
+                           path = paste0(getwd(), "/3_Output"), 
+                           overwrite = overwrite)
+
+# rm(landscapes_medium_ac)
+
+#### 4. High AC ####
+landscapes_high_ac <- readr::read_rds(paste0(getwd(), 
+                                             "/3_Output/landscapes_high_ac_50.rds"))
+
 true_value_high_ac <- clustermq::Q(fun = calculate_lsm_helper,
                                    i = seq_along(landscapes_high_ac), 
                                    const = list(landscape = landscapes_high_ac,
-                                                what = "all"),
+                                                what = c("class", "landscape")),
                                    n_jobs = length(landscapes_high_ac), 
                                    template = list(queue = "mpi", 
                                                    walltime = "06:00", 
                                                    processes = 1))
 
-
-#### 4. Save results ####
-
-overwrite <- FALSE
-
-# Low AC
-UtilityFunctions::save_rds(object = true_value_low_ac,
-                           filename = "true_value_low_ac.rds", 
-                           path = paste0(getwd(), "/3_Output"), 
-                           overwrite = overwrite)
-
-# Medium AC
-UtilityFunctions::save_rds(object = true_value_medium_ac,
-                           filename = "true_value_medium_ac.rds", 
-                           path = paste0(getwd(), "/3_Output"), 
-                           overwrite = overwrite)
-
-# High AC
 UtilityFunctions::save_rds(object = true_value_high_ac,
-                           filename = "true_value_high_ac.rds", 
+                           filename = "true_value_high_ac_50.rds", 
                            path = paste0(getwd(), "/3_Output"), 
                            overwrite = overwrite)
+
+# rm(landscapes_high_ac)
+
--- 2_Experiment/3_run_sampling.R
+++ 2_Experiment/3_run_sampling.R
@@ -3,82 +3,78 @@
 purrr::walk(list.files(path = "1_Setup_Functions", pattern = ".R", full.names = TRUE), 
             function(x) source(x))
 
-#### 2. Import landscapes #### 
+overwrite <- FALSE
 
-# Low AC
+#### 2. Low AC #### 
+
 landscapes_low_ac <- read_rds(paste0(getwd(), 
-                                     "/3_Output/landscapes_low_ac.rds"))
+                                     "/3_Output/landscapes_low_ac_50.rds"))
 
-# Medium AC
-landscapes_medium_ac <- read_rds(paste0(getwd(), 
-                                        "/3_Output/landscapes_medium_ac.rds"))
-
-# High AC
-landscapes_high_ac <- read_rds(paste0(getwd(), 
-                                      "/3_Output/landscapes_high_ac.rds"))
-
-#### 3. Run sampling ####
-
-# Low AC
 sampling_low_ac <- clustermq::Q_rows(fun = sample_plots,
                                      df = simulation_design,
                                      const = list(landscape = landscapes_low_ac, 
-                                                  what = "all"), 
+                                                  what = c("class", "landscape")), 
                                      export = list(sample_circles = sample_circles, 
                                                    sample_squares = sample_squares, 
                                                    sample_rectangle = sample_rectangle),
-                                     n_jobs = nrow(simulation_design), 
+                                     n_jobs = 1500, 
                                      seed = 42,
                                      template = list(queue = "mpi", 
-                                                     walltime = "06:00", 
+                                                     walltime = "48:00", 
                                                      processes = 1))
 
-# Medium AC
+UtilityFunctions::save_rds(object = sampling_low_ac,
+                           filename = "sampling_low_ac_50.rds",
+                           path = paste0(getwd(), "/3_Output"),
+                           overwrite = overwrite)
+
+# rm(landscapes_low_ac)
+
+#### 3. Medium AC ####
+landscapes_medium_ac <- read_rds(paste0(getwd(), 
+                                        "/3_Output/landscapes_medium_ac_50.rds"))
+
 sampling_medium_ac <- clustermq::Q_rows(fun = sample_plots,
                                         df = simulation_design,
                                         const = list(landscape = landscapes_medium_ac, 
-                                                     what = "all"), 
+                                                     what = c("class", "landscape")), 
                                         export = list(sample_circles = sample_circles, 
                                                       sample_squares = sample_squares, 
                                                       sample_rectangle = sample_rectangle),
-                                        n_jobs = nrow(simulation_design), 
+                                        n_jobs = 1500, 
                                         seed = 42,
                                         template = list(queue = "mpi", 
-                                                        walltime = "06:00", 
+                                                        walltime = "48:00", 
                                                         processes = 1))
 
-# High AC
+UtilityFunctions::save_rds(object = sampling_medium_ac,
+                           filename = "sampling_medium_ac_50.rds",
+                           path = paste0(getwd(), "/3_Output"),
+                           overwrite = overwrite)
+
+# rm(landscapes_medium_ac)
+
+#### 4. High AC ####
+landscapes_high_ac <- read_rds(paste0(getwd(), 
+                                      "/3_Output/landscapes_high_ac_50.rds"))
+
 sampling_high_ac <- clustermq::Q_rows(fun = sample_plots,
                                       df = simulation_design,
                                       const = list(landscape = landscapes_high_ac, 
-                                                   what = "all"), 
+                                                   what = c("class", "landscape")), 
                                       export = list(sample_circles = sample_circles, 
                                                     sample_squares = sample_squares, 
                                                     sample_rectangle = sample_rectangle),
-                                      n_jobs = nrow(simulation_design), 
+                                      n_jobs = 1500, 
                                       seed = 42,
                                       template = list(queue = "mpi", 
-                                                      walltime = "06:00", 
+                                                      walltime = "48:00", 
                                                       processes = 1))
 
-
-#### 4. Save Results ####
-
-overwrite <- FALSE
-
-UtilityFunctions::save_rds(object = sampling_low_ac,
-                           filename = "sampling_low_ac.rds",
-                           path = paste0(getwd(), "/3_Output"),
-                           overwrite = overwrite)
-
-UtilityFunctions::save_rds(object = sampling_medium_ac,
-                           filename = "sampling_medium_ac.rds",
-                           path = paste0(getwd(), "/3_Output"),
-                           overwrite = overwrite)
-
 UtilityFunctions::save_rds(object = sampling_high_ac,
-                           filename = "sampling_high_ac.rds",
+                           filename = "sampling_high_ac_50.rds",
                            path = paste0(getwd(), "/3_Output"),
                            overwrite = overwrite)
 
+# rm(landscapes_high_ac)
 

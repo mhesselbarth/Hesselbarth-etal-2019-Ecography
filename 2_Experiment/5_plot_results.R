ee8954eb143f809ce2385c5a8ec023cfa676cd25
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-10-17 12:57:49 +0200

    Update plots

--- 2_Experiment/5_plot_results.R
+++ 2_Experiment/5_plot_results.R
@@ -34,61 +34,46 @@ deviation_complete_ac$autocorrelation <- factor(deviation_complete_ac$autocorrel
 deviation_joined <- dplyr::filter(deviation_complete_ac, level == "landscape") %>% 
   dplyr::left_join(unique(simulation_design[, -5]), 
                    by = c("simulation_id" = "id"), 
-                   suffix = c(".lsm", ".scheme"))
+                   suffix = c("_deviation", "_scheme")) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                   by = "metric", 
+                   suffix = c("_scheme", "_lsm"))
 
 deviation_joined <- dplyr::mutate(deviation_joined, 
-                                  percentage = dplyr::case_when(size ==  2500 & n.scheme == 10 ~ 10,
-                                                                size ==  2500 & n.scheme == 35 ~ 35,
-                                                                size ==  2500 & n.scheme == 75 ~ 75,
-                                                                size ==  7500 & n.scheme == 4 ~ 10,
-                                                                size ==  7500 & n.scheme == 12 ~ 35,
-                                                                size ==  7500 & n.scheme == 25 ~ 75, 
-                                                                size == 20000 & n.scheme == 2 ~ 10,
-                                                                size == 20000 & n.scheme == 5 ~ 35,
-                                                                size == 20000 & n.scheme == 10 ~ 75))
+                                  percentage = dplyr::case_when(size ==  2500 & n_scheme == 10 ~ 10,
+                                                                size ==  2500 & n_scheme == 35 ~ 35,
+                                                                size ==  2500 & n_scheme == 75 ~ 75,
+                                                                size ==  7500 & n_scheme == 4 ~ 10,
+                                                                size ==  7500 & n_scheme == 12 ~ 35,
+                                                                size ==  7500 & n_scheme == 25 ~ 75, 
+                                                                size == 20000 & n_scheme == 2 ~ 10,
+                                                                size == 20000 & n_scheme == 5 ~ 35,
+                                                                size == 20000 & n_scheme == 10 ~ 75))
 
 deviation_summarised <- dplyr::group_by(deviation_joined, 
-                                        autocorrelation, percentage, shape, type.scheme, level, class, metric) %>%
-  dplyr::summarise(correct = mean(inside_ci, na.rm = TRUE), 
-                   false = 1 - correct, 
-                   deviation_rel = mean(deviation_rel, na.rm = TRUE))  %>% 
-  dplyr::mutate(correct_bins = findInterval(correct,
-                                            seq(0, 1, by = 0.1),
-                                            rightmost.closed = TRUE))
-
-deviation_summarised <- dplyr::left_join(deviation_summarised, 
-                                         landscapemetrics::lsm_abbreviations_names, 
-                                         by = "metric")
+                                        autocorrelation, percentage, shape, type_scheme, level, class, metric) %>%
+  dplyr::summarise(mse_rel_mean  = mean(mse_rel_mean, na.rm = TRUE), 
+                   mse_rel_minmax = mean(mse_rel_minmax, na.rm = TRUE), 
+                   rmse_rel_mean  = mean(rmse_rel_mean, na.rm = TRUE), 
+                   rmse_rel_minmax = mean(rmse_rel_minmax, na.rm = TRUE), 
+                   type_lsm = unique(type_lsm))
 
 results_landscape <- dplyr::filter(deviation_summarised, level == "landscape") %>%
   tidyr::unite(unique_label,
-               percentage, shape, type.scheme,
+               percentage, shape, type_scheme,
                remove = FALSE) %>% 
-  dplyr::arrange(type.scheme, shape, percentage)
+  dplyr::arrange(type_scheme, shape, percentage)
 
 results_landscape$unique_label <- factor(results_landscape$unique_label, 
                                          levels = unique(results_landscape$unique_label))
 
-ggplot_metrics <-  ggplot(data = results_landscape, 
-                          aes(x = metric, y = unique_label)) +
-  geom_tile(aes(fill = factor(correct_bins))) + 
-  facet_wrap(~ autocorrelation + type, 
+ggplot_metrics <- ggplot(data = results_landscape, 
+                         aes(x = metric, y = unique_label)) +
+  geom_tile(aes(fill = rmse_rel_mean)) + 
+  facet_wrap(~ autocorrelation + type_lsm, 
              scales = "free_x", 
              ncol = 6, nrow = 3) +
-  scale_fill_brewer(name = "% Correct \nestimation",
-                    type = "div",
-                    palette = "Spectral",
-                    labels = c(
-                      " < 10",
-                      " < 20",
-                      " < 30",
-                      " < 40",
-                      " < 50",
-                      " < 60",
-                      " < 70",
-                      " < 80",
-                      " < 90",
-                      " < 100")) + 
+  scale_fill_viridis_c() + 
   labs(x = "Landscape metrics", y = "Sample scheme") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) #+ 
   # theme_ipsum(axis_title_size = 14)
@@ -106,56 +91,43 @@ deviation_complete_ac$autocorrelation <- factor(deviation_complete_ac$autocorrel
 deviation_joined <- dplyr::filter(deviation_complete_ac, level == "landscape") %>% 
   dplyr::left_join(unique(simulation_design[, -5]), 
                    by = c("simulation_id" = "id"), 
-                   suffix = c(".lsm", ".scheme"))
+                   suffix = c("_deviation", "_scheme")) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                 by = "metric", 
+                 suffix = c("_scheme", "_lsm"))
 
 deviation_joined <- dplyr::mutate(deviation_joined, 
-                                  percentage = dplyr::case_when(size ==  2500 & n.scheme == 10 ~ 10,
-                                                                size ==  2500 & n.scheme == 35 ~ 35,
-                                                                size ==  2500 & n.scheme == 75 ~ 75,
-                                                                size ==  7500 & n.scheme == 4 ~ 10,
-                                                                size ==  7500 & n.scheme == 12 ~ 35,
-                                                                size ==  7500 & n.scheme == 25 ~ 75, 
-                                                                size == 20000 & n.scheme == 2 ~ 10,
-                                                                size == 20000 & n.scheme == 5 ~ 35,
-                                                                size == 20000 & n.scheme == 10 ~ 75))
+                                  percentage = dplyr::case_when(size ==  2500 & n_scheme == 10 ~ 10,
+                                                                size ==  2500 & n_scheme == 35 ~ 35,
+                                                                size ==  2500 & n_scheme == 75 ~ 75,
+                                                                size ==  7500 & n_scheme == 4 ~ 10,
+                                                                size ==  7500 & n_scheme == 12 ~ 35,
+                                                                size ==  7500 & n_scheme == 25 ~ 75, 
+                                                                size == 20000 & n_scheme == 2 ~ 10,
+                                                                size == 20000 & n_scheme == 5 ~ 35,
+                                                                size == 20000 & n_scheme == 10 ~ 75))
 
 deviation_summarised <- dplyr::group_by(deviation_joined, 
-                                        autocorrelation, percentage, shape, type.scheme, level, class, type.lsm) %>%
-  dplyr::summarise(correct = mean(inside_ci, na.rm = TRUE), 
-                   false = 1 - correct, 
-                   deviation_rel = mean(deviation_rel, na.rm = TRUE))  %>% 
-  dplyr::mutate(correct_bins = findInterval(correct,
-                                            seq(0, 1, by = 0.1),
-                                            rightmost.closed = TRUE))
+                                        autocorrelation, percentage, shape, type_scheme, level, class, type_lsm) %>%
+  dplyr::summarise(mse_rel_mean  = mean(mse_rel_mean, na.rm = TRUE), 
+                   mse_rel_minmax = mean(mse_rel_minmax, na.rm = TRUE), 
+                   rmse_rel_mean  = mean(rmse_rel_mean, na.rm = TRUE), 
+                   rmse_rel_minmax = mean(rmse_rel_minmax, na.rm = TRUE))
 
 results_landscape <- dplyr::filter(deviation_summarised, level == "landscape") %>%
   tidyr::unite(unique_label,
-               percentage, shape, type.scheme,
+               percentage, shape, type_scheme,
                remove = FALSE) %>% 
-  dplyr::arrange(type.scheme, shape, percentage)
+  dplyr::arrange(type_scheme, shape, percentage)
 
 results_landscape$unique_label <- factor(results_landscape$unique_label, 
                                          levels = unique(results_landscape$unique_label))
 
 ggplot_type <- ggplot(data = results_landscape, 
-       aes(x = type.lsm,
-           y = unique_label)) +
-  geom_tile(aes(fill = factor(correct_bins))) + 
-  facet_wrap( ~ autocorrelation) +
-  scale_fill_brewer(name = "% Correct \nestimation",
-                    type = "div",
-                    palette = "Spectral",
-                    labels = c(
-                      " < 10",
-                      " < 20",
-                      " < 30",
-                      " < 40",
-                      " < 50",
-                      " < 60",
-                      " < 70",
-                      " < 80",
-                      " < 90",
-                      " < 100")) + 
+                      aes(x = type_lsm, y = unique_label)) +
+  geom_tile(aes(fill = rmse_rel_mean)) + 
+  facet_wrap(~ autocorrelation) +
+  scale_fill_brewer(palette = ) +
   labs(x = "Landscape metrics", y = "Sample scheme") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) #+ 
   # theme_ipsum(axis_title_size = 14)
@@ -174,68 +146,61 @@ deviation_complete_ac$autocorrelation <- factor(deviation_complete_ac$autocorrel
 deviation_joined <- dplyr::filter(deviation_complete_ac, level == "landscape") %>% 
   dplyr::left_join(unique(simulation_design[, -5]), 
                    by = c("simulation_id" = "id"), 
-                   suffix = c(".lsm", ".scheme"))
+                   suffix = c("_deviation", "_scheme")) %>% 
+  dplyr::left_join(landscapemetrics::lsm_abbreviations_names, 
+                   by = "metric", 
+                   suffix = c("_scheme", "_lsm"))
 
 deviation_joined <- dplyr::mutate(deviation_joined, 
-                                  percentage = dplyr::case_when(size ==  2500 & n.scheme == 10 ~ 10,
-                                                                size ==  2500 & n.scheme == 35 ~ 35,
-                                                                size ==  2500 & n.scheme == 75 ~ 75,
-                                                                size ==  7500 & n.scheme == 4 ~ 10,
-                                                                size ==  7500 & n.scheme == 12 ~ 35,
-                                                                size ==  7500 & n.scheme == 25 ~ 75, 
-                                                                size == 20000 & n.scheme == 2 ~ 10,
-                                                                size == 20000 & n.scheme == 5 ~ 35,
-                                                                size == 20000 & n.scheme == 10 ~ 75))
+                                  percentage = dplyr::case_when(size ==  2500 & n_scheme == 10 ~ 10,
+                                                                size ==  2500 & n_scheme == 35 ~ 35,
+                                                                size ==  2500 & n_scheme == 75 ~ 75,
+                                                                size ==  7500 & n_scheme == 4 ~ 10,
+                                                                size ==  7500 & n_scheme == 12 ~ 35,
+                                                                size ==  7500 & n_scheme == 25 ~ 75, 
+                                                                size == 20000 & n_scheme == 2 ~ 10,
+                                                                size == 20000 & n_scheme == 5 ~ 35,
+                                                                size == 20000 & n_scheme == 10 ~ 75))
 
 deviation_summarised <- dplyr::group_by(deviation_joined, 
-                                        autocorrelation, size, n.scheme, shape, type.scheme, level, class, type.lsm) %>%
-  dplyr::summarise(nn = n(),
-    correct = mean(inside_ci, na.rm = TRUE), 
-                   false = 1 - correct, 
-                   deviation_rel = mean(deviation_rel, na.rm = TRUE))  %>% 
-  dplyr::mutate(correct_bins = findInterval(correct,
-                                            seq(0, 1, by = 0.1),
-                                            rightmost.closed = TRUE))
+                                        autocorrelation, percentage, shape, type_scheme, level, class, type_lsm) %>%
+  dplyr::summarise(mse_rel = mean(mse_rel, na.rm = TRUE), 
+                   rmse_rel = mean(rmse_rel, na.rm = TRUE))
 
 results_landscape <- dplyr::filter(deviation_summarised, level == "landscape") %>%
   tidyr::unite(unique_label,
-               size, n.scheme, shape, type.scheme,
+               percentage, shape, type_scheme,
                remove = FALSE) %>% 
-  dplyr::arrange(type.scheme, shape, size, n.scheme,)
+  dplyr::arrange(type_scheme, shape, percentage)
 
 results_landscape$unique_label <- factor(results_landscape$unique_label, 
                                          levels = unique(results_landscape$unique_label))
 
 # Hypothesis 1
-results_landscape %>% 
-  dplyr::group_by(size, n.scheme, autocorrelation, type.lsm) %>%
-  dplyr::summarise(correct = mean(correct), 
-                   false = mean(false)) %>% 
-  dplyr::arrange(autocorrelation, type.lsm, size, n.scheme)
+hypothesis_1_summarised <- results_landscape %>% 
+  dplyr::group_by(autocorrelation, type_lsm, percentage) %>%
+  dplyr::summarise(mse_rel = mean(mse_rel), 
+                   rmse_rel = mean(rmse_rel))
 
 hypothesis_1 <- aov(results_landscape$correct ~ as.factor(results_landscape$percentage))
 summary(hypothesis_1)
 TukeyHSD(hypothesis_1)
 
 # Hypothesis 2
-results_landscape %>% 
-  dplyr::group_by(shape, autocorrelation, type.lsm) %>% 
-  dplyr::summarise(n = n(), 
-                   correct = mean(correct), 
-                   false = mean(false)) %>% 
-  dplyr::arrange(autocorrelation, type.lsm, shape)
+hypothesis_2_summarised <- results_landscape %>% 
+  dplyr::group_by(autocorrelation, type_lsm, shape) %>%
+  dplyr::summarise(mse_rel = mean(mse_rel), 
+                   rmse_rel = mean(rmse_rel))
 
 hypothesis_2 <- aov(results_landscape$correct ~ as.factor(results_landscape$shape))
 summary(hypothesis_2)
 TukeyHSD(hypothesis_2)
 
 # Hypothesis 3
-results_landscape %>% 
-  dplyr::group_by(type.scheme, autocorrelation, type.lsm) %>% 
-  dplyr::summarise(n = n(), 
-                   correct = mean(correct), 
-                   false = mean(false)) %>% 
-  dplyr::arrange(autocorrelation, type.lsm,  type.scheme)
+hypothesis_3_summarised <- results_landscape %>% 
+  dplyr::group_by(autocorrelation,type_lsm, type_scheme)%>%
+  dplyr::summarise(mse_rel = mean(mse_rel), 
+                   rmse_rel = mean(rmse_rel))
 
 random <- dplyr::filter(results_landscape, type.scheme == "random") %>% dplyr::pull(correct)
 regular <- dplyr::filter(results_landscape, type.scheme == "regular") %>% dplyr::pull(correct)
